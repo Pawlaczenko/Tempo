6dfc9c145bb522d4f350f04aa5503f128f2b30a3
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2020-07-11 19:50:03 +0200

    Gameplay works

--- src/js/models/Game.js
+++ src/js/models/Game.js
@@ -4,8 +4,37 @@ export default class Game {
         this.title = title;
         this.artist = artist;
         this.lyrics = lyrics;
-        this.index = [0, 0];
+        this.index = 0;
+        this.time = [0, 0];
 
         this.letters = this.lyrics.split('');
+    }
+
+    changeIndex(n) {
+        if ((n === 1 && this.index < this.letters.length) || (n === -1 && this.index > 0)) {
+            this.index += n;
+        }
+    }
+
+    checkLetter(letter) {
+        return (letter === this.letters[this.index]);
+    }
+
+    checkForEnter() {
+        return (this.letters[this.index] === '\n');
+    }
+
+    ignore(letter) {
+        return !(letter === 'Shift' || letter === 'Control' || letter === 'CapsLock' || letter === 'Alt' || letter === 'AltGraph');
+    }
+
+    timer() {
+        this.time[1] += 1;
+        if (this.time[1] >= 60) {
+            this.time[0] += 1;
+            this.time[1] = 0;
+        }
+
+        return this.time;
     }
 }   
--- src/js/views/gameView.js
+++ src/js/views/gameView.js
@@ -5,7 +5,8 @@ const formatLyrics = lyrics => {
     let markup = '';
     lyrics.forEach((e, i) => {
         let letter = e;
-        letter = (e === '\n') ? `<span class="enter letter" data-index='${i}'>&crarr;</span><br />` : `<span class='letter' data-index='${i}'>${e}</span>`;
+        if (letter === ' ') letter = '&nbsp;'
+        letter = (e === '\n') ? `<span class="enter letter" data-index='${i}'>&crarr;</span><br />` : `<span class='letter' data-index='${i}'>${letter}</span>`;
         markup += letter;
     });
     return markup;
@@ -16,7 +17,7 @@ export const renderGame = song => {
     <div class="game__header">
         <div class="game__title">${song.title} - ${song.artist}</div>
         <p class="game__alert">START TYPING</p>
-        <time class="game__timer">00:00</time>
+        <time class="game__timer" data-time='00:00'>00:00</time>
     </div>
     <div class="game__lyrics">
         ${formatLyrics(song.letters)}
@@ -25,3 +26,39 @@ export const renderGame = song => {
 
     return markup;
 };
+
+export const colorLetter = (index, state) => {
+    const cssClass = (state) ? 'letter--correct' : 'letter--incorrect';
+    const letter = document.querySelector(`[data-index="${index}"]`);
+    if (letter) {
+        letter.classList.add(cssClass);
+    } else {
+        console.log("Letter not found");
+    }
+}
+
+export const deleteLetter = (index) => {
+    const letter = document.querySelector(`[data-index="${index}"]`);
+    const nextLetter = document.querySelector(`[data-index="${index + 1}"]`);
+    const cssClass = `letter ${(letter.classList.contains('enter') ? 'enter' : '')}`;
+    letter.classList = cssClass;
+    nextLetter.classList.remove('letter--active');
+}
+
+export const activateLetter = index => {
+    const letter = document.querySelector(`[data-index="${index}"]`);
+    if (index > 0) {
+        const prevLetter = document.querySelector(`[data-index="${index - 1}"]`);
+        prevLetter.classList.remove('letter--active');
+    }
+    letter.classList.add('letter--active');
+}
+
+export const deleteAlert = () => {
+    document.querySelector('.game__alert').style.display = 'none';
+}
+
+export const updateTime = (time) => {
+    let clock = `${(time[0] < 10) ? '0' : ''}${time[0]}:${(time[1] < 10) ? '0' : ''}${time[1]}`;
+    document.querySelector('.game__timer').innerHTML = clock;
+}
