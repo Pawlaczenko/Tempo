b22c1c920ebd78d65c042995bd9cdbb86843f50f
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2020-07-14 12:53:24 +0200

    Refactorig index.js

--- src/js/index.js
+++ src/js/index.js
@@ -15,11 +15,14 @@ window.state = state;
  */
 
 const controlSearch = id => {
-    // console.log(id);
+    //Get query from input
     const query = searchView.getInput(id);
+
     if (query) {
-        state.page = `search?q=${query}&page=${1}`;
-        window.location.hash = state.page;
+        //Set current Page
+        window.location.hash = `search?q=${query}&page=${1}`;
+
+        //onHashChange will do the rest
     }
 }
 
@@ -30,15 +33,20 @@ const searchHandler = async (query, page) => {
     common.clearMain();
     common.renderLoader(common.elements.main);
     try {
+        //Get search results
         await state.search.getResults(page);
+        //Get number of sites
         state.search.calcSites();
         console.log(state.search.results);
+
+        //Prepare the UI
         common.deleteLoader();
         searchView.clearInput();
-        console.log(state.page);
         if (state.page.includes('search')) {
+            //Render search views
             common.renderView('search', searchView.renderResults(state.search.results, state.search.query, page, state.search.sitesQnt));
-            addQueries();
+            //Add Event Listeners
+            addSearchListeners();
         }
     } catch (error) {
         console.log(error);
@@ -46,52 +54,160 @@ const searchHandler = async (query, page) => {
     }
 }
 
+const addSearchListeners = () => {
+    document.querySelector('.search__results').addEventListener('click', e => {
+        //Get Target
+        const target = e.target.closest('.song');
+        //Check if song can be played
+        if (target && !target.classList.contains('song--disabled')) {
+            //Get track id
+            const id = target.dataset.goto;
+            //Find the track object
+            const chosen = state.search.results.find(e => {
+                return e.track.track_id === parseInt(id, 10);
+            });
+            //Open game
+            gameControl(chosen.track);
+        }
+    });
+}
+
 window.addEventListener('submit', e => {
     e.preventDefault();
-    controlSearch(e.target.id.substr(2));
+    //Get Input ID
+    const id = e.target.id.substr(2);
+    //Call Controller
+    controlSearch(id);
 });
 
-// [common.elements.searchForm, common.elements.searchFormA].forEach(item => {
-//     console.log(item.classList);
-//     // item.addEventListener('submit',controlSearch());
-// });
+/**
+ * Game Controller
+ */
 
+const gameHandler = (e) => {
+    //Get pressed key
+    let key = e.key;
+    //Check if key is not ignored
+    if (state.game.ignore(key)) {
+        if (key === 'Enter' && state.game.checkForEnter()) {
+            //Color green
+            gameView.colorLetter(state.game.index, 1);
+            //change index
+            state.game.changeIndex(1);
+        } else if (key === 'Backspace') {
+            //Change index
+            state.game.changeIndex(-1);
+            //Set cursor
+            gameView.activateLetter(state.game.index);
+            //Delete letter
+            gameView.deleteLetter(state.game.index);
+
+            //If letter is pressed
+        } else {
+            if (state.game.checkLetter(key)) {
+                gameView.colorLetter(state.game.index, 1);
+                state.game.changeIndex(1);
+            } else {
+                gameView.colorLetter(state.game.index, 0);
+                state.game.changeIndex(1);
+            }
+        }
+        //Set cursor
+        gameView.activateLetter(state.game.index);
+    }
+}
+
+const gameControl = async song => {
+    console.log(song);
+    window.location.hash = 'game';
+    //Create new game object
+    state.game = new Game(song.track_id, song.track_name, song.artist_name);
+
+    //Prepare the UI
+    common.clearMain();
+    common.renderLoader(common.elements.main);
+    try {
+        //Get Lyrics
+        await state.game.getLyrics();
+        //Render View
+        common.deleteLoader();
+        common.renderView('game', gameView.renderGame(state.game));
+        //Render cursor
+        gameView.activateLetter(0);
+        //Add event listeners
+        window.addEventListener('keypress', intitGame, { once: true });
+        window.addEventListener('keydown', gameHandler);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const intitGame = (e) => {
+    gameView.deleteAlert();
+    state.timer = window.setInterval(() => {
+        gameView.updateTime(state.game.timer());
+    }, 1000);
+}
 
 /**
  * NAVIGATION CONTROL
  */
 
 const navigationControl = () => {
+    //Get hash
     state.page = window.location.hash.replace('#', '');
+
+    //Clear timer and events if needed
     if (state.timer) window.clearInterval(state.timer);
     if (state.page !== 'game') window.removeEventListener('keydown', gameHandler);
 
-    if (state.page && (state.page === 'home' || state.page === 'about')) {
+
+    // if (state.page && (state.page === 'home' || state.page === 'about')) {
+    //     common.clearMain();
+
+    //     switch (state.page) {
+    //         case 'home': {
+    //             state.page = 'home';
+    //             common.renderView(state.page, homeView.renderHome());
+    //             break;
+    //         }
+    //         case 'about': {
+    //             state.page = 'about';
+    //             common.renderView(state.page, aboutView.renderAbout());
+    //             break;
+    //         }
+    //     }
+    // } else if (state.page.includes('search')) {
+    //     let params = new URLSearchParams(state.page.replace('search', ''));
+
+    //     let query = params.get('q');
+    //     let page = (params.get('page')) ? parseInt(params.get('page'), 10) : 1;
+    //     console.log(query + ' - ' + page);
+    //     if (query) searchHandler(query, page);
+    // } else if (state.page === '') {
+    //     state.page = 'home';
+    //     common.clearMain();
+    //     common.elements.main.classList.add('home');
+    //     common.renderView('home', homeView.renderHome());
+    // }
+    if (state.page) {
         common.clearMain();
+        if (state.page === 'home' || state.page === 'about') {
+            // Render View
+            common.renderView(state.page, window[`${state.page}View`].window[`render${state.page.charAt(0).toUpperCase()}${state.page.slice(1)}`]);
+        } else if (state.page.includes('search')) {
+            //Get parameters
+            let params = new URLSearchParams(state.page.replace('search', ''));
+            let query = params.get('q');
+            let page = (params.get('page')) ? parseInt(params.get('page'), 10) : 1;
 
-        switch (state.page) {
-            case 'home': {
-                state.page = 'home';
-                common.renderView(state.page, homeView.renderHome());
-                break;
-            }
-            case 'about': {
-                state.page = 'about';
-                common.renderView(state.page, aboutView.renderAbout());
-                break;
-            }
+            //Call Search Handler
+            if (query) searchHandler(query, page);
         }
-    } else if (state.page.includes('search')) {
-        let params = new URLSearchParams(state.page.replace('search', ''));
-
-        let query = params.get('q');
-        let page = (params.get('page')) ? parseInt(params.get('page'), 10) : 1;
-        console.log(query + ' - ' + page);
-        if (query) searchHandler(query, page);
+        //If page is empty
     } else if (state.page === '') {
         state.page = 'home';
         common.clearMain();
-        common.elements.main.classList.add('home');
         common.renderView('home', homeView.renderHome());
     }
 }
@@ -106,76 +222,3 @@ const resetPage = () => {
 
 window.addEventListener('hashchange', navigationControl);
 window.addEventListener('load', resetPage);
-
-
-/**
- * Game Controller
- */
-
-const gameHandler = (e) => {
-    let key = e.key;
-    if (state.game.ignore(key)) {
-        if (key === 'Enter' && state.game.checkForEnter()) {
-            gameView.colorLetter(state.game.index, 1);
-            state.game.changeIndex(1);
-        } else if (key === 'Backspace') {
-            console.log('backhello');
-            state.game.changeIndex(-1);
-            gameView.activateLetter(state.game.index);
-            gameView.deleteLetter(state.game.index);
-        } else {
-            if (state.game.checkLetter(key)) {
-                gameView.colorLetter(state.game.index, 1);
-                state.game.changeIndex(1);
-            } else {
-                gameView.colorLetter(state.game.index, 0);
-                state.game.changeIndex(1);
-            }
-        }
-        gameView.activateLetter(state.game.index);
-    }
-}
-
-const gameControl = async song => {
-    console.log(song);
-    state.hash = 'game';
-    window.location.hash = state.hash;
-    state.game = new Game(song.track_id, song.track_name, song.artist_name);
-    common.clearMain();
-    common.renderLoader(common.elements.main);
-    try {
-        console.log('tutaj w try');
-        await state.game.getLyrics();
-        //Render View
-        common.deleteLoader();
-        common.renderView('game', gameView.renderGame(state.game));
-        gameView.activateLetter(0);
-        state.line = 0;
-        window.addEventListener('keypress', intitGame, { once: true });
-        window.addEventListener('keydown', gameHandler);
-    } catch (error) {
-        console.log('pumperni' + error);
-    }
-}
-
-const intitGame = (e) => {
-    gameView.deleteAlert();
-    state.timer = window.setInterval(() => {
-        gameView.updateTime(state.game.timer());
-    }, 1000);
-}
-
-
-const addQueries = () => {
-    document.querySelector('.search__results').addEventListener('click', e => {
-        const target = e.target.closest('.song');
-        if (target && !target.classList.contains('song--disabled')) {
-            const id = target.dataset.goto;
-            console.log(id);
-            const chosen = state.search.results.find(e => {
-                return e.track.track_id === parseInt(id, 10);
-            });
-            gameControl(chosen.track);
-        }
-    });
-}
